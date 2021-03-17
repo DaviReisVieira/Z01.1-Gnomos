a376fdc32cf342d0352baeec2b60ca37a9c748bf
Rosada-eng <guilhermer4@al.insper.edu.br>
2021-03-16 23:07:31 -0300

    mux16 ok

--- Projetos/B-LogicaCombinacional/src/Mux2Way.vhd
+++ Projetos/B-LogicaCombinacional/src/Mux2Way.vhd
@@ -12,5 +12,8 @@ end entity;
 architecture arch of Mux2Way is
 begin
 
-
+	q <= '1' when (sel = '0' AND a = '1') else
+		 '0' when (sel = '0' AND a = '0') else
+		 '1' when (sel = '1' AND b = '1') else
+		 '0';
 end architecture;
