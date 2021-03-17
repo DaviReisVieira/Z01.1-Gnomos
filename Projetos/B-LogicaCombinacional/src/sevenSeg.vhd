38effb7a1f12cf7871a02602cf9c317e860a8f24
Lihsayuri <liviasm1@al.insper.edu.br>
2021-03-17 17:58:39 -0300

    Adicionando meus exercicios

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -10,4 +10,19 @@ end entity;
 architecture arch of sevenSeg is
 begin
 
+with bcd select
+
+
+leds <= "0000001" when "0000", -- numero 0
+		  "1001111" when "0001", -- numero 1 
+		  "0101101" when "0010", -- numero 2
+		  "0000110" when "0011", -- numero 3
+		  "1001100" when "0100", --numero 4
+		  "0100100" when "0101", --numero 5
+		  "0100000" when "0110", -- numero 6
+		  "0001111" when "0111", --numero 7
+		  "0000000" when "1000", --numero 8
+		  "0000100" when "1001", --numero 9
+		  "1111111" when others; --Leds desligados.
+
 end architecture;
