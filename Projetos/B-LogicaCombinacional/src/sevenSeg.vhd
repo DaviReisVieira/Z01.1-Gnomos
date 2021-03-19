6719becb37cee461fb003931de780f4980189a90
Lihsayuri <liviasm1@al.insper.edu.br>
2021-03-18 21:44:24 -0300

    SevenSeg atualizado

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -14,15 +14,21 @@ with bcd select
 
 
 leds <= "0000001" when "0000", -- numero 0
-		  "1001111" when "0001", -- numero 1 
-		  "0101101" when "0010", -- numero 2
-		  "0000110" when "0011", -- numero 3
-		  "1001100" when "0100", --numero 4
-		  "0100100" when "0101", --numero 5
-		  "0100000" when "0110", -- numero 6
-		  "0001111" when "0111", --numero 7
-		  "0000000" when "1000", --numero 8
-		  "0000100" when "1001", --numero 9
-		  "1111111" when others; --Leds desligados.
+		"1001111" when "0001", -- numero 1 
+		"0101101" when "0010", -- numero 2
+		"0000110" when "0011", -- numero 3
+		"1001100" when "0100", --numero 4
+		"0100100" when "0101", --numero 5
+		"0100000" when "0110", -- numero 6
+		"0001111" when "0111", --numero 7
+		"0000000" when "1000", --numero 8
+		"0000100" when "1001", --numero 9
+		"0000010" when "1010", --digito A
+		"1100000" when "1011", -- digito B
+		"1110010" when "1100", -- digito C
+		"1000010" when "1101", --digito D
+		"0110000" when "1110", -- digito E
+		"0111000" when "1111", -- digito F
+		"1111111" when others; --Leds desligados.
 
 end architecture;
