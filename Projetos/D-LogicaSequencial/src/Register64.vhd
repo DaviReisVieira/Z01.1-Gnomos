d645aabf2fc06ddf3fc9a8f2f34d89a3bab38f6b
jamessonlps <jamesson1233@gmail.com>
2021-04-21 22:24:58 -0300

    implementa register64 com imagens de onda e rtl

--- Projetos/D-LogicaSequencial/src/Register64.vhd
+++ Projetos/D-LogicaSequencial/src/Register64.vhd
@@ -7,10 +7,12 @@ use ieee.std_logic_1164.all;
 
 entity Register64 is
 	port(
+		-- entradas
 		clock:   in STD_LOGIC;
-		input:   in STD_LOGIC_VECTOR(63 downto 0);
+		input:   in STD_LOGIC_VECTOR(63 downto 0); -- vetor 64 bits
 		load:    in STD_LOGIC;
-		output: out STD_LOGIC_VECTOR(63 downto 0)
+		-- saída
+		output: out STD_LOGIC_VECTOR(63 downto 0)  -- vetor 64 bits
 	);
 end entity;
 
@@ -18,13 +20,33 @@ architecture arch of Register64 is
 
 	component Register32 is
 		port(
+			-- entradas
 			clock:   in STD_LOGIC;
-			input:   in STD_LOGIC_VECTOR(31 downto 0);
+			input:   in STD_LOGIC_VECTOR(31 downto 0); -- vetor 32 bits
 			load:    in STD_LOGIC;
-			output: out STD_LOGIC_VECTOR(31 downto 0)
+			-- saída
+			output: out STD_LOGIC_VECTOR(31 downto 0)  -- vetor 32 bits
       );
 	end component;
 
 begin
+
+	-- Registrador 1 de 32 bits
+	register1: Register32
+	port map (
+		clock => clock,
+		load => load,
+		input => input(31 downto 0),
+		output => output(31 downto 0)
+	);
+	
+	-- Registrador 2 de 32 bits
+	register2: Register32
+	port map (
+		clock => clock,
+		load => load,
+		input => input(63 downto 32),
+		output => output(63 downto 32)
+	);
 
 end architecture;
